7e33545fe6025f62c63c90ab7f1ff73f6e403a8e
Alex Rudy <alex.rudy@gmail.com>
2015-08-07 04:45:25 -1000

    Adjusted for new project KOPy

--- KOPy/__init__.py
+++ KOPy/__init__.py
@@ -1,7 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
 """
-This is an Astropy affiliated package.
+KOPy - Keck Observing Python
 """
 
 # Affiliated packages may add whatever they like to this file, but
