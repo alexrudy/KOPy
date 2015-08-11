43cdd50a94cc1ea3b13b04073a0fb36bccf4c67d
Alex Rudy <alex.rudy@gmail.com>
2015-08-10 16:07:09 -1000

    KOPy Target OO interface

---
+++ KOPy/targets.py
@@ -0,0 +1,218 @@
+# -*- coding: utf-8 -*-
+#
+#  targets.py
+#  KOPy
+#
+#  Created by Alexander Rudy on 2015-08-07.
+#  Copyright 2015 Alexander Rudy. All rights reserved.
+#
+"""
+:mod:`targets` provides an object-oriented way to work with starlists in the Keck starlist format.
+"""
+
+import six
+import astropy.units as u
+import collections
+import io
+import numpy as np
+from astropy.coordinates import SkyCoord, Angle
+from astropy.table import Table, Column, MaskedColumn
+from .starlist import parse_starlist
+
+class Target(object):
+    """A single target object, with a position, name, and keyword arguments."""
+    
+    keywords = {}
+    
+    def __init__(self, name, position, _keywords=dict(), **kwargs):
+        super(Target, self).__init__()
+        self.name = str(name)
+        self.position = SkyCoord(position)
+        self.keywords = collections.OrderedDict()
+        self.keywords.update(_keywords)
+        self.keywords.update(kwargs)
+        
+        
+    def __getattr__(self, key):
+        """Delegate attributes to keywords when necessary."""
+        if key in self.keywords:
+            return self.keywords[key]
+        raise AttributeError("'{:s}' has no attribute '{:s}'".format(self.__class__.__name__, key))
+        
+    def __repr__(self):
+        """Represent a target."""
+        return "<{0:s} '{1:s}'@'{2:s}' {3:s}>".format(self.__class__.__name__, self.name, self.pos_string(), ", ".join(self._repr_keywords_()))
+        
+    def __eq__(self, other):
+        """Equality."""
+        return (self.name == other.name) and (self.position == other.position)
+        
+    def _repr_keywords_(self):
+        """Clean representation of keywords."""
+        if not len(self.keywords):
+            return [""]
+        keywords = []
+        for key, value in self.keywords.items():
+            if "mag" == key[-3:]:
+                keywords.insert(0, "{key}={value:.2f}".format(key=key, value=float(value)))
+            else:
+                value = six.text_type(value)
+                if " " in value:
+                    value = "'" + value + "'"
+                keywords.append("{key:s}={value:s}".format(key=key, value=value))
+        return keywords
+        
+    def pos_string(self):
+        """Position string, in a nicely formatted way."""
+        position = self.position.transform_to('icrs')
+        string = "{ra:s} {dec:s}".format(
+            ra = position.ra.to_string(u.hourangle, precision=3, pad=True),
+            dec = position.dec.to_string(u.deg, precision=3, pad=True, alwayssign=True),
+            )
+        return string
+        
+    def to_starlist(self):
+        """Return a starlist line."""
+        position = self.position.transform_to('fk5')
+        name = six.text_type(self.name).strip().replace(" ","_")
+        line = "{name:<15.15s} {ra:s} {dec:s} {epoch:.0f} {keywords:s}".format(
+                                name = name,
+                                ra = position.ra.to_string(u.hourangle, sep=' ', precision=3, pad=True),
+                                dec = position.dec.to_string(u.deg, sep=" ", precision=3, pad=True, alwayssign=True),
+                                epoch = position.equinox.jyear,
+                                keywords = " ".join(self._repr_keywords_()))
+        return line
+    
+
+class TargetList(collections.MutableSequence, list):
+    """A target list"""
+    def __init__(self, iterable=None):
+        super(TargetList, self).__init__()
+        if iterable is not None:
+            self.extend(iterable)
+    
+    @classmethod
+    def _type_check(cls, value):
+        """Type check the value."""
+        if not isinstance(value, Target):
+            raise TypeError("{0:s} must contain only subclasses of {1:s}".format(
+                cls.__name__, Target.__name__
+            ))
+        return value
+    
+    def __setitem__(self, key, value):
+        """Ensure type consistency!"""
+        return list.__setitem__(self, key, self._type_check(value))
+    
+    def __getitem__(self, key):
+        """Ensure that we always get back a TargetList"""
+        
+        # Support indexing by name.
+        if isinstance(key, six.string_types):
+            for t in self:
+                if t.name == key:
+                    return t
+            else:
+                raise KeyError("No target with name '{0:s}' found".format(key))
+        
+        # Support regular list indexing, but turn it into a TargetList
+        # if we would otherwise return a list.
+        r = list.__getitem__(self, key)
+        if isinstance(r, list):
+            return self.__class__(r)
+        return r
+        
+    def __add__(self, item):
+        """Add two TargetList objects together."""
+        return self.__class__(super(TargetList, self).__add__(item))
+        
+    def __mul__(self, value):
+        """Multiply"""
+        return self.__class__(super(TargetList, self).__mul__(value))
+    
+    def __rmul__(self, value):
+        """Reverse multiply."""
+        return self.__class__(super(TargetList, self).__rmul__(value))
+        
+    def __len__(self):
+        """Length, from the underlying list."""
+        return list.__len__(self)
+        
+    def insert(self, index, item):
+        """Insert an item, and check type."""
+        return list.insert(self, index, self._type_check(item))
+    
+    @classmethod
+    def from_starlist(cls, filename):
+        """From a starlist"""
+        return cls(Target(name, position, _keywords=kw) for name, position, kw in parse_starlist(filename))
+    
+    @classmethod
+    def from_table(cls, table):
+        """Reconstruct the starlist from a table created by self.table()"""
+        if "Name" in table.colnames:
+            names = table['Name']
+            table.remove_column('Name')
+        else:
+            raise ValueError("Table must have a 'Name' column.")
+        if "Position" in table.colnames:
+            positions = SkyCoord(table['Position'])
+            table.remove_column('Position')
+        elif "RA" in table.colnames and "Dec" in table.colnames:
+            positions = SkyCoord(table['RA'], table['Dec'])
+            table.remove_columns(['RA', 'Dec'])
+        else:
+            raise ValueError("Table must have either 'Position' or 'RA' and 'Dec' columns.")
+        return cls(Target(name, position, **{k:row[k] for k in row.colnames if row[k] is not np.ma.masked }) for name, position, row in zip(names, positions, table))
+    
+    @property
+    def names(self):
+        """Target names."""
+        return [ t.name for t in self ]
+        
+    def catalog(self):
+        """Make a single SkyCoord object for all targets."""
+        return SkyCoord([t.position.transform_to('icrs') for t in self])
+        
+    def table(self, coord_mixin=False):
+        """Create a table object which represents this target list."""
+        
+        cols = collections.OrderedDict()
+        def _make_row(t):
+            d = collections.OrderedDict(Name=t.name)
+            d.update(t.keywords)
+            cols.update((k, None) for k in d.keys())
+            return d
+        
+        
+        data = [ _make_row(t) for t in self ]
+        columns = []
+        for c in cols:
+            ma = np.ma.MaskedArray([d.get(c, np.ma.masked) for d in data])
+            columns.append(MaskedColumn(ma, name=c, mask=ma.mask))
+        
+        t = Table(columns, masked=True)
+        if coord_mixin:
+            t['Position'] = self.catalog()
+            t['Position'].format = lambda c : c.to_string('hmsdms')
+        else:
+            catalog = self.catalog()
+            t.add_column(MaskedColumn(catalog.ra.to(u.hourangle), name='RA', format=lambda c : Angle(c).to_string(), mask=np.zeros(catalog.shape, dtype=np.bool)), index=1)
+            t.add_column(MaskedColumn(catalog.dec, name="Dec", format=lambda c : Angle(c).to_string(), mask=np.zeros(catalog.shape, dtype=np.bool)), index=2)
+        return t
+        
+    def to_starlist_stream(self, file):
+        """Write to a stream"""
+        for t in self:
+            file.write(t.to_starlist())
+            file.write("\n")
+        
+    def to_starlist(self, filename, mode='w'):
+        """Write to a starlist file."""
+        if isinstance(filename, io.IOBase) or hasattr(filename, 'write'):
+            stream = filename
+            self.to_starlist_stream(stream)
+        else:
+            with open(filename, mode) as stream:
+                self.to_starlist_stream(stream)
+        
---
+++ KOPy/tests/test_targetlist.py
@@ -0,0 +1,112 @@
+import pytest
+
+import os
+
+from ..targets import Target, TargetList
+from astropy.tests.helper import assert_quantity_allclose
+from astropy.coordinates import SkyCoord
+from six.moves import StringIO
+
+@pytest.fixture
+def targetlist():
+    """An example target list, quite simple"""
+    import pkg_resources
+    return TargetList.from_starlist(pkg_resources.resource_filename(__name__, 'data/small_starlist.txt'))
+
+@pytest.fixture
+def target():
+    """A target option."""
+    return Target(name="MyTarget", position="1h4m3s +5d4m9s")
+
+def test_read_starlist(starlist_filename):
+    """Read a starlist filename."""
+    tl = TargetList.from_starlist(starlist_filename)
+    assert len(tl)
+    
+def test_index_starlist(targetlist):
+    """Test indexing into a starlist."""
+    assert len(targetlist) == 6
+    
+    s = targetlist[1:3]
+    assert len(s) == 2
+    assert isinstance(s, TargetList)
+    
+    t = targetlist[1]
+    assert isinstance(t, Target)
+    
+    t = targetlist['S2CM006571']
+    assert t.name == 'S2CM006571'
+    
+    with pytest.raises(KeyError):
+        targetlist['OTHERNAME']
+    
+def test_setitem_starlist(targetlist, target):
+    """Test target item starlist."""
+    targetlist[1] = target
+    assert targetlist[1].name == target.name
+    
+def test_starlist_type_enforcement():
+    """Test type enforcement."""
+    
+    with pytest.raises(TypeError):
+        tl = TargetList([1])
+        
+    tl = TargetList()
+    with pytest.raises(TypeError):
+        tl.append(1)
+        
+    tl = TargetList()
+    with pytest.raises(TypeError):
+        tl.extend([1,2,3])
+        
+def test_starlist_add(targetlist, target):
+    """Test addition of starlists."""
+    tl = targetlist + []
+    assert isinstance(tl, TargetList)
+    
+    tl = targetlist + [target]
+    assert isinstance(tl, TargetList)
+    
+    with pytest.raises(TypeError):
+        tl = targetlist + [1]
+        
+def test_starlist_mul(target):
+    """Test starlist multiplication."""
+    tl = TargetList([target]) * 2
+    assert isinstance(tl, TargetList)
+    assert len(tl) == 2
+    
+    tl = 4 * TargetList([target])
+    assert isinstance(tl, TargetList)
+    assert len(tl) == 4
+    
+def test_starlist_names(target):
+    """Test target names."""
+    tl = TargetList([target, target])
+    assert tl.names == [target.name, target.name]
+    
+def assert_coord_allclose(actual, desired, rtol=1.e-7, atol=None):
+    """Check that coordinates are allclose."""
+    actual = SkyCoord(actual)
+    desired = SkyCoord(desired)
+    assert_quantity_allclose(actual.ra, desired.ra, rtol=rtol, atol=atol)
+    assert_quantity_allclose(actual.dec, desired.dec, rtol=rtol, atol=atol)
+    assert_quantity_allclose(actual.distance, desired.distance, rtol=rtol, atol=atol)
+    
+def test_starlist_catalog(target):
+    """Test starlist catalog"""
+    tl = TargetList([target, target])
+    assert_coord_allclose(tl.catalog(), SkyCoord([target.position, target.position]))
+    
+def test_targetlist_table_roundtrip(targetlist):
+    """Round trip via table."""
+    tl = TargetList.from_table(targetlist.table())
+    assert tl.names == targetlist.names
+    
+def test_targetlist_starlist_roundtrip(targetlist, tmpdir):
+    """Target list round trip via starlist."""
+    targetlist.to_starlist(str(tmpdir.join('starlist.txt')))
+    tl = TargetList.from_starlist(str(tmpdir.join('starlist.txt')))
+    assert tl.names == targetlist.names
+    
+    
